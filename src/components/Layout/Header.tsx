8a8e77ab97ef9c8fc30ce25fca222335be0be03e
saddam <saddam@vaultspay.ae>
2022-10-14 02:13:57 +0500

    fixes

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react"
 
 
 export default function Header(){
-    return (
+    const [colapse,setColapse] = useState<any>('')
+	const handleColapse = ()=>{
+		if(colapse == ''){
+			setColapse('show')
+		}else{
+			setColapse('')
+		}
+	}
+	return (
+	
         <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar ftco-navbar-light site-navbar-target" id="ftco-navbar">
 	    <div className="container">
-	      <a className="navbar-brand" href="index.html">Muhammad Saddam</a>
+	      <a className="navbar-brand" href="https://msaddamkamal.github.io/">M.Saddam</a>
 	      <button className="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-	        <span className="oi oi-menu"></span> Menu
+	        <span className="oi oi-menu text-primary"></span> Menu
 	      </button>
+	      {/* <button className="" type="button" onClick={handleColapse}>
+	        <span className="oi oi-menu text-primary"></span> Menu
+	      </button> */}
 
 	      <div className="collapse navbar-collapse" id="ftco-nav">
 	        <ul className="navbar-nav nav ml-auto">
