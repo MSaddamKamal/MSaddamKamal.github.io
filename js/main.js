2853170ea467292fe32fab4b567e178938865ce4
saddam <saddam@vaultspay.ae>
2022-10-14 02:16:34 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -135,41 +135,41 @@
 
 	// scroll
 	var scrollWindow = function() {
-		$(window).scroll(function(){
-			var $w = $(this),
-					st = $w.scrollTop(),
-					navbar = $('.ftco_navbar'),
-					sd = $('.js-scroll-wrap');
+		// $(window).scroll(function(){
+		// 	var $w = $(this),
+		// 			st = $w.scrollTop(),
+		// 			navbar = $('.ftco_navbar'),
+		// 			sd = $('.js-scroll-wrap');
 
-			if (st > 150) {
-				if ( !navbar.hasClass('scrolled') ) {
-					navbar.addClass('scrolled');	
-				}
-			} 
-			if (st < 150) {
-				if ( navbar.hasClass('scrolled') ) {
-					navbar.removeClass('scrolled sleep');
-				}
-			} 
-			if ( st > 350 ) {
-				if ( !navbar.hasClass('awake') ) {
-					navbar.addClass('awake');	
-				}
+		// 	if (st > 150) {
+		// 		if ( !navbar.hasClass('scrolled') ) {
+		// 			navbar.addClass('scrolled');	
+		// 		}
+		// 	} 
+		// 	if (st < 150) {
+		// 		if ( navbar.hasClass('scrolled') ) {
+		// 			navbar.removeClass('scrolled sleep');
+		// 		}
+		// 	} 
+		// 	if ( st > 350 ) {
+		// 		if ( !navbar.hasClass('awake') ) {
+		// 			navbar.addClass('awake');	
+		// 		}
 				
-				if(sd.length > 0) {
-					sd.addClass('sleep');
-				}
-			}
-			if ( st < 350 ) {
-				if ( navbar.hasClass('awake') ) {
-					navbar.removeClass('awake');
-					navbar.addClass('sleep');
-				}
-				if(sd.length > 0) {
-					sd.removeClass('sleep');
-				}
-			}
-		});
+		// 		if(sd.length > 0) {
+		// 			sd.addClass('sleep');
+		// 		}
+		// 	}
+		// 	if ( st < 350 ) {
+		// 		if ( navbar.hasClass('awake') ) {
+		// 			navbar.removeClass('awake');
+		// 			navbar.addClass('sleep');
+		// 		}
+		// 		if(sd.length > 0) {
+		// 			sd.removeClass('sleep');
+		// 		}
+		// 	}
+		// });
 	};
 	scrollWindow();
 
